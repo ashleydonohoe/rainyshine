86a3fdd451d8500dd40746b1a1610b8faed26010
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-20 22:01:21 -0400

    added layout for main screen and assets

--- RainyShine/WeatherVC.swift
+++ RainyShine/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  RainyShine
 //
 //  Created by Gabriele on 9/19/16.
@@ -9,18 +9,12 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
